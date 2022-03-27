0aae283c3f33ed614aae20f683e346fac65a24b7
Fabricio Martins <fabriciocsmartins@gmail.com>
2022-03-27 18:59:25 -0300

    [feat] Adiciona footer na pag

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './style.css';
+
+function Footer() {
+
+  return(
+    <footer className="footerInfos">
+      <div>
+        <p>Criado por: Fabrício Martins</p>
+      </div>
+      <div>
+        <p>Contato: <a href="https://www.linkedin.com/in/fabr%C3%ADcio-costa-da-silveira-martins-4a1405140/">Linkedin</a> / <a href="https://github.com/FabricioCSM">GitHub</a></p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
