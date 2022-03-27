30f55a489437587eb4db206de844ed37d4673b19
Fabricio Martins <fabriciocsmartins@gmail.com>
2022-03-27 13:29:28 -0300

    [feat] implementa componente header na aplicação

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+import AppContext from "../../context/AppContext";
+import './styles.css';
+
+function Header (){
+
+  const [citySearch, setCitySearch] = useState('')
+  const { currentWeatherSearch } = useContext(AppContext);
+
+  const handleChange = ({target}) => {
+    setCitySearch(target.value)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    currentWeatherSearch(citySearch);
+  }
+
+
+  return(
+    <main>
+      <header className="header">
+        <div style={{ display: 'flex', flexDirection: "column" }}>
+          <form className="col-12" style={{width: '100%',display: 'flex', flexDirection: 'row'}}>
+            <input
+              type="search"
+              placeholder="Insert a city name"
+              className="form-control form-control-dark"
+              onChange={handleChange}
+              aria-label="Search"
+            ></input>
+            <button
+              className="btn btn-warning"
+              onClick={ handleSearch }
+            >Search</button>
+          </form>
+        </div>
+      </header>
+    </main>
+  )
+}
+
+export default Header;
